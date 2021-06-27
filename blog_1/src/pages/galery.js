3a13b65897344e13936972cb42067bc867ff3dc3
Marek <marek.becht@gmail.com>
2021-06-27 12:32:32 +0200

    dodanie linków komuminacji na stronie

---
+++ blog_1/src/pages/galery.js
@@ -0,0 +1,23 @@
+import * as React from "react"
+import {Link} from 'gatsby' 
+
+const galery = () => {
+
+return (
+
+<div>
+  <h1>
+   Galeria zdięć
+  </h1> 
+  <p>Moje prace</p>
+  <p><Link to="/">Home</Link></p>
+</div>
+
+
+)
+
+}
+
+
+
+export default galery
