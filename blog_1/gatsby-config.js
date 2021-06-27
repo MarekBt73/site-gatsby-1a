2edefa52affbd532d999c83348bfa763e3aa778c
Marek <marek.becht@gmail.com>
2021-06-27 14:27:14 +0200

    iii

--- blog_1/gatsby-config.js
+++ blog_1/gatsby-config.js
@@ -2,5 +2,17 @@ module.exports = {
   siteMetadata: {
     title: "blog1",
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud",
+
+  {
+    resolve: `gatsby-plugin-typography`,
+    options: {
+      pathToConfigModule: `src/utils/typography`,
+    },
+  },
+
+],
+
+
 };
+
