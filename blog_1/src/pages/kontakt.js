877e235a97da8bae40bd729db53380740430f10b
Marek <marek.becht@gmail.com>
2021-06-27 12:02:21 +0200

    next2

---
+++ blog_1/src/pages/kontakt.js
@@ -0,0 +1,21 @@
+import * as React from "react"
+import {Link} from 'gatsby' 
+
+const contactPage = () => {
+
+return (
+
+<div>
+  <h1>
+   Kontakt
+  </h1>
+</div>
+
+
+)
+
+}
+
+
+
+export default contactPage
---
+++ blog_1/src/pages/omnie.js
@@ -0,0 +1,21 @@
+import * as React from "react"
+import {Link} from 'gatsby' 
+
+const oMnie = () => {
+
+return (
+
+<div>
+  <h1>
+   O mnie
+  </h1>
+</div>
+
+
+)
+
+}
+
+
+
+export default oMnie
